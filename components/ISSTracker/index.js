665803e5f6e9f9f9bc29cc507fe0327b173f7dda
Umay Soyluer <umaysoyluer@gmail.com>
2023-09-22 12:04:37 +0200

    fetching and error handling with SWR

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,45 +1,51 @@
-import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
+  const fetcher = async (url) => {
+    const res = await fetch(URL);
+
+    if (!res.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+
+    return res.json();
+  };
+
+  const { data, isLoading, error, mutate } = useSWR(URL, fetcher, {
+    refreshInterval: 5000,
   });
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
   return (
-    <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
-      <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
-      />
-    </main>
+    <>
+      {data && (
+        <>
+          {" "}
+          <Map longitude={data.longitude} latitude={data.latitude} />
+          <Controls
+            longitude={data.longitude}
+            latitude={data.latitude}
+            onRefresh={() => {
+              mutate();
+            }}
+          />{" "}
+        </>
+      )}
+      {error && (
+        <>
+          <h2>Something went wrong ...</h2>
+          <span>
+            {error.status} : {error.message}
+          </span>
+        </>
+      )}
+      {isLoading && <h2>Loading ...</h2>}
+    </>
   );
 }
